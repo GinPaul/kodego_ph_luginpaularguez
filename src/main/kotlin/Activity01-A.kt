998ba6ae7c09f890a31c25d361a4fa60b41e64a9
GinPaul <luginlpp@gmail.com>
2022-11-08 19:23:46 +0800

    Nov 8 2022 commit

---
+++ src/main/kotlin/Activity01-A.kt
@@ -0,0 +1,52 @@
+package assignments
+
+fun main(){
+    println()
+    var studentNumber : Int = 2022001
+    var studentName1 : String = "Lugin Paul Arguez"
+    var age1 : Int = 37
+    var sex1 : String = "Male"
+    var address1 : String = "Caloocan City"
+    var isEnrolled1 : Boolean = true
+    var section1 : Char = 'A'
+    var emailAddress1 : String = "luginlpp@gmail.com"
+    var mobileNumber1 : Long = 639171234567
+    var currentScore1 : Double = 99.99
+    //current score not automatically computed
+
+    println("Student Number: $studentNumber")
+    println("Full Name: $studentName1")
+    println("Age: $age1")
+    println("Sex: $sex1")
+    println("Address: $address1")
+    println("Is Enrolled?: $isEnrolled1")
+    println("Section Name: $section1")
+    println("Email Address?: $emailAddress1")
+    println("Mobile Number: $mobileNumber1")
+    println("Current Score: $currentScore1")
+
+    println()
+
+    var studentNumber2 : Int = 2022002
+    var studentName2 : String = "Susan dela Cruz"
+    var age2 : Int = 40
+    var sex2 : String = "Female"
+    var address2 : String = "Quezon City"
+    var isEnrolled2 : Boolean = false
+    var section2 : Char = 'B'
+    var emailAddress2 : String = "s.delacruz@gmail.com"
+    var mobileNumber2 : Long = 639208889999
+    var currentScore2 : Double = 98.00
+
+    println("Student Number: $studentNumber2")
+    println("Student Name: $studentName2")
+    println("Age: $age2")
+    println("Sex: $sex2")
+    println("Address: $address2")
+    println("Is Enrolled?: $isEnrolled2")
+    println("Section Name: $section2")
+    println("Email Address?: $emailAddress2")
+    println("Mobile Number: $mobileNumber2")
+    println("Current Score: $currentScore2")
+
+}
